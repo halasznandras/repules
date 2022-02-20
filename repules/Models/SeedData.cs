20a556d6958eac773c28dbdd76059a8d9ef593f9
AndrasHalasz <halasz.nandras@gmail.com>
2022-02-21 00:35:25 +0100

    Add project files.

---
+++ repules/Models/SeedData.cs
@@ -0,0 +1,28 @@
+﻿using repules.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace repules.Models
+{
+    public class SeedData
+    {
+        public static void Inicializalas(IServiceProvider serviceProvider)
+        {
+            AppDbContext db = new AppDbContext(ServiceProvider.GetRequiredService<DbContextOptions<AppDbContext>>());
+            if (!db.Pilotak.Any())
+            {
+                var sorok = File.ReadAllLines(@"C:\temp\pilotak.csv").Skip(1);
+                foreach (var sor in sorok)
+                {
+                    db.Pilotak.Add(new pilotak(sor));
+                }
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ repules/Models/pilotak.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace repules.Models
+{
+    public class pilotak
+    {
+        public int Id { get; set; }
+        [Display(Name = "Név")]
+        public string Nev { get; set; }
+        [Display(Name = "Dátum")]
+        public int SzulDatum { get; set; }
+        [Display(Name = "Nemzetiség")]
+        public string Nemzet { get; set; }
+        [Display(Name = "Rajtszám")]
+        public string Rajtszam { get; set; }
+        public pilotak()
+        { }
+
+        public pilotak(string sor)
+        {
+            string[] adat = sor.Split(";");
+            Nev = adat[0];
+            SzulDatum = Convert.ToInt32(adat[1]);
+            Nemzet = adat[2];
+            Rajtszam = adat[3];
+        }
+    }
+}
